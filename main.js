1892aee3adfc5ea8975f280814d733fe2d5555de
Mohamad Almasri <44051605+mhdalmasri@users.noreply.github.com>
2019-03-04 14:14:20 +0100

    months deleted

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ class soon {
     const now = moment([]);
     const next = moment(['2020']);
     const diff = next.diff(now, 'seconds');
-    const months = Math.floor(diff / 2629746)
-    const days = Math.floor((diff % 2629746) / 86400)
-    const hours= Math.floor(((diff % 2629746) % 86400)/3600)
-    const minutes =Math.floor((((diff % 2629746) % 86400)%3600)/60)
-    const seconds= (((diff % 2629746) % 86400)%3600)%60
+    const days = Math.floor(diff / 86400)
+    const hours = Math.floor((diff % 86400) / 3600)
+    const minutes= Math.floor(((diff % 86400) % 3600)/60)
+    const seconds =(((diff % 86400) % 3600)%3600)%60
+    
 
     console.log(now.format('LLL'));
     console.log(next.format('LLL'));
     console.log(diff);
-    console.log(months);
+  
     console.log(days);
     console.log(hours);
     console.log(minutes);
     console.log(seconds);
 
-    time.textContent = `${months} Months ${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`
+    time.textContent = ` ${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`
   }
 
 }
