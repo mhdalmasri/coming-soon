1a793812bd41e72ca38cbfaf0abf2ee81f93056b
Mohamad Almasri <44051605+mhdalmasri@users.noreply.github.com>
2019-03-05 14:24:34 +0100

    some lines deleted

--- main.js
+++ main.js
@@ -5,25 +5,19 @@ class soon {
   init() {
     let time = document.getElementById("js");
     const now = moment([]);
-    const next = moment(['2020']);
+    console.log(now.toArray());
+    const next = moment(['2020, 0, 1, 0, 0, 0, 0']);
+    console.log(next.toArray());
     const diff = next.diff(now, 'seconds');
     const days = Math.floor(diff / 86400)
     const hours = Math.floor((diff % 86400) / 3600)
-    const minutes= Math.floor(((diff % 86400) % 3600)/60)
-    const seconds =(((diff % 86400) % 3600)%3600)%60
+    const minutes = Math.floor(((diff % 86400) % 3600) / 60)
+    const seconds = (((diff % 86400) % 3600) % 3600) % 60
     time.textContent = ` ${days} Days .. ${hours} Hours .. ${minutes} Minutes .. ${seconds} Seconds ..`
 
-    console.log(now.format('LLL'));
-    console.log(next.format('LLL'));
-    console.log(diff);
-    console.log(days);
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);
-
-    
   }
-
 }
 
-setInterval(()=>{const newSoon = new soon()},1000)
+setInterval(() => {
+  const newSoon = new soon()
+}, 1000)
